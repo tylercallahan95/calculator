852fa7620cb0da4bc860b7fa8c988977f88acf9a
Tyler Callahan <tylercallahan95@gmail.com>
2025-07-05 16:28:17 -0400

    feat: Initial build of frontend

--- calc.js
+++ calc.js
@@ -38,3 +38,33 @@ const operate = function (num, num2, operator) {
 };
 
 operate(num, num2, operator);
+
+// Create function to create button
+// Use function in loop to do 0-9
+// Use function to then make the operator buttons as well (+, -, *, /, =, x or back)
+
+const display = document.getElementById('display');
+display.style.display = 'inline-block';
+display.style.border = 'solid';
+display.style.fontSize = '20px';
+display.style.padding = '5px';
+display.style.margin = '10px';
+
+function button(i) {
+  buttonExample = document.createElement('button');
+  buttonExample.appendChild(document.createTextNode(i));
+  div = document.getElementById('btn');
+  div.appendChild(buttonExample);
+}
+
+for (let i = 0; i <= 9; i++) {
+  button(i);
+}
+
+// const operators = ['+', '-', '*', '/', '=', 'x'];
+button('+');
+button('-');
+button('*');
+button('/');
+button('=');
+button('x');
